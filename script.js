2306a08480f328a44b6a9df3f66497707644e6a4
Johannes Stenfeldt <johannesstenfeldt@gmail.com>
2023-01-27 20:14:28 +0100

    fetchade en till API för att se om det fungerar

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
+const APIKey = "UQ0nHj3wQkHLj52bjcjrk1QF8WtKmUmUqCSIMQSA"
+
 // document.getElementById("content");
 
-async function GetNasaInfo() {
-    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=UQ0nHj3wQkHLj52bjcjrk1QF8WtKmUmUqCSIMQSA")
+// https://api.nasa.gov/planetary/apod?api_key=UQ0nHj3wQkHLj52bjcjrk1QF8WtKmUmUqCSIMQSA
+
+// let url = "https://images-api.nasa.gov/search?q=space"
+
+// const search = document.querySelector("#search")
+// const button = document.querySelector("button")
+
+// button.addEventListener("click", () => {
+
+//     const search = document.querySelector("#search")
+
+//     url = `https://images-api.nasa.gov/search?q=`
+
+
+//     fetch(url).then(response => response.json()).then((data) => {
+//         console.log("data", data);
+//     });
+// });
+
+
+
+
+async function APOD() {
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${APIKey}`)
     const info = await response.json()
     console.log(info)
     // document.getElementById("content").textContent = info.date
-    document.getElementById("content").innerHTML = `${info.date} <img>${info.hdurl}</img>`
+    document.getElementById("content").innerHTML = `<img src="${info.url}" width="200px"><p>${info.title}</p>${info.date}<p>${info.explanation}</p>`
 }
 
-GetNasaInfo()
+APOD()
 
-
-
+async function Asteroids() {
+    const response = await fetch (`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${APIKey}`)
+    const info = await response.json()
+    console.log(info)
+}
 
 
 
